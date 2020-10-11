22ffd4926b5b51c9a464084174cc934a76445fe6
Christopher Bergman <christopher.bergman@learnet.se>
2020-10-11 10:49:47 +0200

    Har lagt till en class parser, Gjort en main för förenkla Unit testing samt skrivit en snabb TODO.

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 
 
+/* TODO 
+ * Få en string input T.ex: "two plus two" och översätta till både integers samt operator och sedan skriva ut vad summan blir.
+ * Skapa UnitTests som ger "godkänt" när man har lyckats fixa ihop tillräcklig med kod för att få det att fungera.
+ * */
+
 namespace TeamProject
 {
+
    public static class Program
-    {
-       public static int PrettyParse(string text) //Startar med att skapa en metod som returnerar 0, sedan skapar vi X antal tests och startar kodningen därefter.
+    { 
+
+
+         public static string PrettyParse(string text)
+         {
+        return $"{text} = {Parse(text)}";
+         }
+
+    
+        public static double Parse(string text) //Startar med att skapa en metod som returnerar 0, sedan skapar vi X antal tests och startar kodningen därefter.
         {
             return 0;
         }
+
     }   
 }
+ 
--- XunitTest/UnitTest1.cs
+++ XunitTest/UnitTest1.cs
@@ -10,7 +10,13 @@ namespace XunitTest
         [Fact]
         public void Addition()
         {
-            Assert.Equal(5, TeamProject.Program.PrettyParse("Two plus Three"));  //Första test gällande addition.
+            Assert.Equal(5, TeamProject.Program.Parse("two plus three"));  //Första test gällande addition. 
+        }
+
+        [Fact]
+        public void BiggerAddition() // Tänker att man även testar en string som har en längre addition för att se så att vi inte bara får rätt när man har två int och en operator.
+        {
+            Assert.Equal(15, TeamProject.Program.Parse("five plus five plus five"));
         }
     }
 }
