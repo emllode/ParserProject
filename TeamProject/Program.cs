2c86c0a7df5f14f69bc511c3878a0cb6d703a7bc
Christopher Bergman <christopher.bergman@learnet.se>
2020-10-11 10:53:17 +0200

    Lyckats göra så testen blir misslyckade, nu kan vi starta skriva kod för få tests att fungera.

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -1,27 +1,14 @@
 ﻿using System;
 
-
-/* TODO 
- * Få en string input T.ex: "two plus two" och översätta till både integers samt operator och sedan skriva ut vad summan blir.
- * Skapa UnitTests som ger "godkänt" när man har lyckats fixa ihop tillräcklig med kod för att få det att fungera.
- * */
-
 namespace TeamProject
 {
-
+    // Skapar separat "main" där vi har bör fixa en main som kan lösa UniTests.
    public static class Program
     { 
 
-
-         public static string PrettyParse(string text)
-         {
-        return $"{text} = {Parse(text)}";
-         }
-
-    
-        public static double Parse(string text) //Startar med att skapa en metod som returnerar 0, sedan skapar vi X antal tests och startar kodningen därefter.
+        static void Main(string[] args)
         {
-            return 0;
+            Console.WriteLine(Parser.PrettyParse("text"));
         }
 
     }   
--- XunitTest/UnitTest1.cs
+++ XunitTest/UnitTest1.cs
@@ -10,13 +10,13 @@ namespace XunitTest
         [Fact]
         public void Addition()
         {
-            Assert.Equal(5, TeamProject.Program.Parse("two plus three"));  //Första test gällande addition. 
+            Assert.Equal(5, Parser.Parse("two plus three"));  //Första test gällande addition. 
         }
 
         [Fact]
         public void BiggerAddition() // Tänker att man även testar en string som har en längre addition för att se så att vi inte bara får rätt när man har två int och en operator.
         {
-            Assert.Equal(15, TeamProject.Program.Parse("five plus five plus five"));
+            Assert.Equal(15, Parser.Parse("five plus five plus five"));
         }
     }
 }
