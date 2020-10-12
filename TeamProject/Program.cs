6694faf277efcdf71d5800914068c8c71c961a25
Christopher Bergman <christopher.bergman@learnet.se>
2020-10-12 08:43:16 +0200

    lagt till lite kommentarer bara.

--- TeamProject/Program.cs
+++ TeamProject/Program.cs
@@ -2,13 +2,13 @@
 
 namespace TeamProject
 {
-    // Skapar separat "main" där vi har bör fixa en main som kan lösa UniTests.
+    // Skapar separat "main" där vi har så vi kan få UnitTests att fungera.
    public static class Program
     { 
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Parser.PrettyParse("text"));
+            Console.WriteLine(Parser.PrettyParse("text")); //random för att få unitTests att funka, de krävs en static main.
         }
 
     }   
