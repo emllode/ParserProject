0096ec93b2c08acf5bcd851462c634f7c8d1067f
Christopher Bergman <christopher.bergman@learnet.se>
2020-10-15 11:28:28 +0200

    La till en förklaring på debug inuti unittests.

--- XunitTest/UnitTest1.cs
+++ XunitTest/UnitTest1.cs
@@ -7,6 +7,20 @@ namespace XunitTest
 {
     public class UnitTest1
     {
+        // --- > En beskrivning på en debug NEDANFÖR <---------------
+
+        /* Har gjort samtliga tests nedan, lyckades med två av tre. Det viktigaste var att få till plus samt minus skulle fungera och ifall jag hade tid/ville så skulle jag
+         * även utveckla så jag kunde ha en längre string med siffror + operatörer och ge ut svaret. Men jag tänkte att det var bäst att klara av 2 values samt 1 operatör
+         * fört. Så jag valde då helt enkelt att köra two plus three samt seven minus two.
+         * 
+         * En bugg som jag mötte på var att alla inputs multiplicerades med 2. dvs: "two plus two" fick ett svar som 8. Detta löste jag genom att i min kod så stod det först:
+         * "results += calculate;" När jag väl tog bort +  så fungerade det utmärkt. Vet inte riktigt vrför den ville multplicera men jag lyckades iaf "debugga" genom att ta bort/lägga
+         * enstaka grejer.
+         * 
+         * 
+         * 
+         * 
+         */
         [Fact]
         public void Plus()
         {
